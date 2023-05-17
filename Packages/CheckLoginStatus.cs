f3e1c774a067658635ba839e2c08559b0c10d94f
Bartosz Lemieszko <bartosz.lemieszko@gmail.com>
2023-05-17 21:59:00 +0200

    XD

---
+++ Packages/CheckLoginStatus.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SaginPortal.Packages; 
+
+public class CheckLoginStatusAttribute : ActionFilterAttribute {
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var session = context.HttpContext.Session;
+        if (!session.Keys.Contains("UserId"))
+        {
+            context.Result = new RedirectToActionResult("Login", "Account", null);
+        }
+    }
+
+}
