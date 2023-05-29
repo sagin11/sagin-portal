bf13812c79b499c608311033f88c54c95b40aa82
Bartosz Lemieszko <bartosz.lemieszko@gmail.com>
2023-05-30 00:37:35 +0200

    Kosmetycznie zmiany w AddQuestion i EditQuestion

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -14,23 +14,21 @@ public class QuestionsController : Controller {
     }
     
     [HttpGet]
+    [CheckLoginStatus]
     [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
     [Route("/Dashboard/Exam/{id:int}/Edit/Questions/AddQuestion")]
     public async Task<IActionResult> AddQuestion(int id = -1) {
         ViewBag.ExamId = id;
         var model = new AddQuestionModel();
         model.Answers = new List<AnswerModel>();
-        return View(model);
+        ViewBag.formUrl = $"/Dashboard/Exam/{ViewBag.ExamId}/Edit/Questions/AddQuestion";
+        return View($"~/Views/Questions/AddEditQuestion.cshtml", model);
     }
     
-    // TODO: Zmienić AddQuestionPost na AddQuestion w widoku
     [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
     [HttpPost]
-    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/AddQuestionPost")]
+    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/AddQuestion")]
     public async Task<IActionResult> AddQuestionPost(AddQuestionModel model, int id = -1) {
-        
-        // TODO: Sprawdzanie czy jakakolwiek odpowiedź jest zaznaczona jako poprawna
-        // TODO: Rework całej metody bo nie działa xd
         var answersList = model.Answers;
         foreach (var answer in answersList) {
             if (answer.Content == "" || answer.Content == null) return StatusCode(400);           
@@ -62,31 +60,6 @@ public class QuestionsController : Controller {
         return Redirect("/Dashboard/Exam/" + id + "/Edit");
     }   
 
-    
-    [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
-    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/")]
-    public async Task<IActionResult> Questions(int id = -1) {
-        var questions = await _dbContext.Questions.Where(q => q.ExamId == id).ToListAsync();
-        ViewBag.questions = questions;
-        var answers = await _dbContext.Answers.Where(q => q.ExamId == id).ToListAsync();
-        ViewBag.answers = answers;
-
-        return View();
-    }
-    
-    [HttpGet]
-    [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
-    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/Delete/{questionId:int}")]
-    public async Task<IActionResult> DeleteQuestion(int id = -1, int questionId = -1) {
-        var question = await _dbContext.Questions.FirstOrDefaultAsync(q => q.Id == questionId);
-        var answers = await _dbContext.Answers.Where(a => a.QuestionId == questionId).ToListAsync();
-        _dbContext.Questions.Remove(question);
-        foreach (var answer in answers) {
-            _dbContext.Answers.Remove(answer);
-        }
-        await _dbContext.SaveChangesAsync();
-        return Redirect($"/Dashboard/Exam/{id}/Edit/Questions");
-    }
 
     [HttpGet]
     [CheckLoginStatus]
@@ -108,7 +81,9 @@ public class QuestionsController : Controller {
         ViewBag.ExamId = id;
         ViewBag.question = question;
         ViewBag.answers = answers;
-        return View(model);
+        
+        ViewBag.formUrl = $"/Dashboard/Exam/{ViewBag.ExamId}/Edit/Questions/AddQuestion";
+        return View($"~/Views/Questions/AddEditQuestion.cshtml", model);
     }
 
     [HttpPost]
@@ -152,6 +127,31 @@ public class QuestionsController : Controller {
         await _dbContext.SaveChangesAsync();
 
         return Redirect("/Dashboard/Exam/" + id + "/Edit");
+    }
+    
+    [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
+    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/")]
+    public async Task<IActionResult> Questions(int id = -1) {
+        var questions = await _dbContext.Questions.Where(q => q.ExamId == id).ToListAsync();
+        ViewBag.questions = questions;
+        var answers = await _dbContext.Answers.Where(q => q.ExamId == id).ToListAsync();
+        ViewBag.answers = answers;
+
+        return View();
+    }
+    
+    [HttpGet]
+    [ServiceFilter(typeof(ExamExistsValidatorAttribute))]
+    [Route("/Dashboard/Exam/{id:int}/Edit/Questions/Delete/{questionId:int}")]
+    public async Task<IActionResult> DeleteQuestion(int id = -1, int questionId = -1) {
+        var question = await _dbContext.Questions.FirstOrDefaultAsync(q => q.Id == questionId);
+        var answers = await _dbContext.Answers.Where(a => a.QuestionId == questionId).ToListAsync();
+        _dbContext.Questions.Remove(question);
+        foreach (var answer in answers) {
+            _dbContext.Answers.Remove(answer);
+        }
+        await _dbContext.SaveChangesAsync();
+        return Redirect($"/Dashboard/Exam/{id}/Edit/Questions");
     }
 
 }
